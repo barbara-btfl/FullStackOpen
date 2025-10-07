ce69d788755a980a571999a8c31f4170210000df
BARBARA PETERS <barbara.peters@gmail.com>
2025-10-07 13:19:19 +0200

    Commit exercise 1.1

--- Part1/part1/src/App.jsx
+++ Part1/part1/src/App.jsx
@@ -1,3 +1,7 @@
+import { Header } from "./Header";
+import { Content } from "./Content";
+import { Total } from "./Total";
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
