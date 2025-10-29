6c14a4bf8a9481f3680ecda441f7e7dbbb69732d
BARBARA PETERS <barbara.peters@gmail.com>
2025-10-29 11:01:41 +0100

    Commit 2.6

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -4,20 +4,42 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName("");
+
+    console.log("button clicked", event.target);
+  };
+
   return (
-    <div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      ...
-    </div>
+    <>
+      <div>
+        <h2>Phonebook</h2>
+        <form onSubmit={addPerson}>
+          <div>
+            name:{" "}
+            <input
+              value={newName}
+              onChange={(e) => setNewName(e.target.value)}
+            />
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </form>
+        <h2>Numbers</h2>
+        <ul>
+          {persons.map((person) => (
+            <li key={person.name}>{person.name}</li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
