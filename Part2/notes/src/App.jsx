966829a7f3a88344b4a326cbf062ace1c8b0da2b
BARBARA PETERS <barbara.peters@gmail.com>
2025-10-29 15:09:28 +0100

    Notes PAL 2d changing importance

--- Part2/notes/src/App.jsx
+++ Part2/notes/src/App.jsx
@@ -38,6 +38,16 @@ const App = () => {
     ? notes
     : notes.filter((note) => note.important === true);
 
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    axios.put(url, changedNote).then((response) => {
+      setNotes(notes.map((note) => (note.id === id ? response.data : note)));
+    });
+  };
+
   return (
     <div>
       <h1>Notes</h1>
@@ -48,7 +58,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
