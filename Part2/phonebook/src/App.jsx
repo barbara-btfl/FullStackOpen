28496a3777273563a44179b3f108864c0a32bcb7
BARBARA PETERS <barbara.peters@gmail.com>
2025-10-29 15:48:12 +0100

    Commit 2.12 and 2.13

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import personService from "./services/persons";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
@@ -11,10 +11,8 @@ const App = () => {
   const [newNumber, setNewNumber] = useState("");
 
   useEffect(() => {
-    console.log("effect");
-    axios.get("http://localhost:3001/persons").then((response) => {
-      console.log("promise fulfilled");
-      setPersons(response.data);
+    personService.getAll().then((initialPersons) => {
+      setPersons(initialPersons);
     });
   }, []);
 
@@ -26,11 +24,11 @@ const App = () => {
       id: persons.length + 1, // Add id for new persons
     };
 
-    setPersons(persons.concat(personObject));
-    setNewName("");
-    setNewNumber("");
-
-    console.log("button clicked", event.target);
+    personService.create(personObject).then((returnedPerson) => {
+      setPersons(persons.concat(returnedPerson));
+      setNewName("");
+      setNewNumber("");
+    });
   };
 
   return (
