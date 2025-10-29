bc29bbf5b9802720b7a0f881d309ec3ae3ea5742
BARBARA PETERS <barbara.peters@gmail.com>
2025-10-29 13:36:12 +0100

    Commit 2.8

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
     const personObject = {
       name: newName,
+      number: newNumber,
     };
 
     setPersons(persons.concat(personObject));
     setNewName("");
+    setNewNumber("");
 
     console.log("button clicked", event.target);
   };
@@ -40,13 +45,30 @@ const App = () => {
             />
           </div>
           <div>
+            number:{" "}
+            <input
+              value={newNumber}
+              // Check if number is only using numbers and dashes
+              onChange={(e) => {
+                const value = e.target.value;
+                if (/^[0-9-]*$/.test(value)) {
+                  setNewNumber(value);
+                } else {
+                  alert("Invalid number format");
+                }
+              }}
+            />
+          </div>
+          <div>
             <button type="submit">add</button>
           </div>
         </form>
         <h2>Numbers</h2>
         <ul>
           {persons.map((person) => (
-            <li key={person.name}>{person.name}</li>
+            <li key={person.name}>
+              {person.name} {person.number}
+            </li>
           ))}
         </ul>
       </div>
