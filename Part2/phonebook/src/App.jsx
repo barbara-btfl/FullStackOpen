0822aed03fb95ad9d5f270b67c0086b5a9662413
BARBARA PETERS <barbara.peters@gmail.com>
2025-10-30 09:53:25 +0100

    Commit 2.16

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -3,12 +3,15 @@ import personService from "./services/persons";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import { Notification } from "./components/Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [filter, setFilter] = useState("");
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
 
   useEffect(() => {
     personService.getAll().then((initialPersons) => {
@@ -22,9 +25,18 @@ const App = () => {
         .remove(id)
         .then(() => {
           setPersons(persons.filter((person) => person.id !== id));
+          setSuccessMessage(`Deleted ${name}`);
+          setTimeout(() => {
+            setSuccessMessage(null);
+          }, 5000);
         })
         .catch(() => {
-          alert(`The person '${name}' was already deleted from server`);
+          setErrorMessage(
+            `The person '${name}' was already deleted from server`
+          );
+          setTimeout(() => {
+            setErrorMessage(null);
+          }, 5000);
           setPersons(persons.filter((person) => person.id !== id));
         });
     }
@@ -44,8 +56,18 @@ const App = () => {
                 person.id === existingPerson.id ? returnedPerson : person
               )
             );
+            setSuccessMessage(`Updated ${newName}'s number`);
+            setTimeout(() => {
+              setSuccessMessage(null);
+            }, 5000);
             setNewName("");
             setNewNumber("");
+          })
+          .catch(() => {
+            setErrorMessage(`Failed to update ${newName}'s information`);
+            setTimeout(() => {
+              setErrorMessage(null);
+            }, 5000);
           });
       }
     } else {
@@ -54,16 +76,30 @@ const App = () => {
         number: newNumber,
       };
 
-      personService.create(personObject).then((returnedPerson) => {
-        setPersons(persons.concat(returnedPerson));
-        setNewName("");
-        setNewNumber("");
-      });
+      personService
+        .create(personObject)
+        .then((returnedPerson) => {
+          setPersons(persons.concat(returnedPerson));
+          setSuccessMessage(`Added ${newName}`);
+          setTimeout(() => {
+            setSuccessMessage(null);
+          }, 5000);
+          setNewName("");
+          setNewNumber("");
+        })
+        .catch(() => {
+          setErrorMessage(`Failed to add ${newName}`);
+          setTimeout(() => {
+            setErrorMessage(null);
+          }, 5000);
+        });
     }
   };
 
   return (
     <div>
+      <Notification message={errorMessage} type="error" />
+      <Notification message={successMessage} type="success" />
       <h2>Phonebook</h2>
 
       <Filter filter={filter} onFilterChange={setFilter} />
---
+++ Part2/phonebook/src/components/Notification.jsx
@@ -0,0 +1,9 @@
+export const Notification = ({ message, type = "error" }) => {
+  if (message === null) {
+    return null;
+  }
+
+  const className = type === "success" ? "success" : "error";
+
+  return <div className={className}>{message}</div>;
+};
