b00c0c4ea5a8d89a35dc13bf4e99dd13982dfdd4
BARBARA PETERS <barbara.peters@gmail.com>
2025-11-12 14:41:32 +0100

    Part 3b - PAL 2

--- Part3/index.js
+++ Part3/index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.static("dist"));
 
 let notes = [
   {
@@ -30,12 +32,7 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-app.use(express.json());
 app.use(requestLogger);
-
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
-});
 
 app.get("/api/notes", (request, response) => {
   response.json(notes);
