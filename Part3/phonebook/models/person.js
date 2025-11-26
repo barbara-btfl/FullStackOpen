60093729c64c2a8762271b025ad60f200c8abccf
BARBARA PETERS <barbara.peters@gmail.com>
2025-11-26 14:21:54 +0100

    Commit 3.20

--- Part3/phonebook/models/person.js
+++ Part3/phonebook/models/person.js
@@ -20,7 +20,22 @@ const personSchema = new mongoose.Schema({
     minlength: 3,
     required: true,
   },
-  number: String,
+  // validation for phone number: at least 8 characters, must contain a hyphen on 3rd of 4th position
+
+  number: {
+    type: String,
+    minlength: 8,
+    required: true,
+    validate: {
+      validator: function (v) {
+        // Phone number must have exactly one hyphen after 2 or 3 digits
+        // Examples: 09-1234556, 040-22334455, but not 0401-234556 or 09--1234556
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: (props) =>
+        `${props.value} is not a valid phone number! Phone number must have a hyphen after 2 or 3 digits.`,
+    },
+  },
 });
 
 personSchema.set("toJSON", {
